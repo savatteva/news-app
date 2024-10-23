cc4de88d27decb0b2b34902e9215167b1d5ae374
Polina Savateeva <polina.savateeva@geogracom.com>
2024-10-23 15:54:26 +0300

    git init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Main from "./pages/Main/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Main />
+      </div>
+    </>
+  );
+};
+
+export default App;
