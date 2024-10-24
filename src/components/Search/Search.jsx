22ae06e71e24a4650f8f82433eb404bcf3bca6ea
Polina Savateeva <polina.savateeva@geogracom.com>
2024-10-24 16:44:48 +0300

    added search

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,17 @@
+import styles from "./Search.module.css";
+
+const Search = ({ keywords, setKeywords }) => {
+  return (
+    <div className={styles.search}>
+      <input
+        type="text"
+        value={keywords}
+        className={styles.input}
+        onChange={(e) => setKeywords(e.target.value)}
+        placeholder={"javascript"}
+      />
+    </div>
+  );
+};
+
+export default Search;
